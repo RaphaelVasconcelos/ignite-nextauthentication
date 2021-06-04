638a21a13b5d660c5bfc620a32e02d57b0c4b6f9
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-06-04 05:58:58 -0300

    Adding disconect user

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import Router from "next/router";
-import { setCookie, parseCookies } from "nookies";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import React, { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshtoken");
+
+  Router.push("/");
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
@@ -34,11 +41,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const { "nextauth.token": token } = parseCookies();
 
     if (token) {
-      api.get("/me").then((response) => {
-        const {email, permissions, roles} = response.data
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
 
-        setUser ({email, permissions, roles})
-      });
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
     }
   }, []);
 
@@ -62,7 +74,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       setUser({ email, permissions, roles });
 
-      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (error) {
